d4ed6f649cf6d0aa15efea3314561354d6f0817a
zapkingston <zapkingston@gmail.com>
2026-02-20 09:11:05 -0500

    added temp folder

--- js.js
+++
@@ -1,8 +0,0 @@
-let letNumber = 102; // a number
-let letFloat = 67; // a decimal (float)
-let letBoolean = true; // a boolean
-let letString = "Hello World!"; // a string
-let letArray = []; // an empty array
-let letObject = {}; // an empty object
-let letNull = null; // a null value
-let letUndefined = undefined; // an undefined value
