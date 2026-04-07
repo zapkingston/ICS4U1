1368f23e4112c8b6d35f8d0e7533b699e8c34437
zapkingston <zapkingston@gmail.com>
2026-04-06 23:31:17 -0400

    Change paragraph class in CubicEquation component
    Updated the paragraph class for styling.

--- src/Components/CubicEquation.tsx
+++ src/Components/CubicEquation.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function CubicEquation({ a, b, c, d }: Props) {
   return (
-    <p className="Equation">
+    <p className="font-bold mb-3 text-lg">
       y = {a}x³ + {b}x² + {c}x + {d}
     </p>
   );
